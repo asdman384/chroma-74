3e3fa856264b6fe0fb1600cbe1ff150312a8555e
Oleg <13420499+asdman384@users.noreply.github.com>
2025-10-27 22:56:01 +0200

    Update WEATHER_URL

--- src/config.example.h
+++ src/config.example.h
@@ -10,7 +10,7 @@ const char PROGMEM *WIFI_PASSWORD_2 = "";
 // API endpoints and keys
 const char PROGMEM AQI_REQUEST[] = "{\"location\": {\"latitude\": ,\"longitude\": },\"extraComputations\": [\"POLLUTANT_CONCENTRATION\"]}";
 const char PROGMEM AQI_URL[] = "https://airquality.googleapis.com/v1/currentConditions:lookup?key=";
-const char PROGMEM WEATHER_URL[] = "https://weather.googleapis.com/v1/currentConditions:lookup?location.latitude=50.018973&location.longitude=19.906229&key=";
+const char PROGMEM WEATHER_URL[] = "https://weather.googleapis.com/v1/currentConditions:lookup?location.latitude=1&location.longitude=1&key=";
 
 // Hardware configuration
 #define BATTERY_PIN 35             // GPIO35 for battery voltage monitoring
