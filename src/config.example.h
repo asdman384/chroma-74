713d2d64f01ced9e109c233d257ca9d0b76cb6cb
Oleg <13420499+asdman384@users.noreply.github.com>
2025-10-27 22:55:22 +0200

    Modify AQI_REQUEST
    Updated AQI_REQUEST

--- src/config.example.h
+++ src/config.example.h
@@ -8,7 +8,7 @@ const char PROGMEM *WIFI_SSID_2 = "";
 const char PROGMEM *WIFI_PASSWORD_2 = "";
 
 // API endpoints and keys
-const char PROGMEM AQI_REQUEST[] = "{\"location\": {\"latitude\": 50.018843,\"longitude\": 19.906014},\"extraComputations\": [\"POLLUTANT_CONCENTRATION\"]}";
+const char PROGMEM AQI_REQUEST[] = "{\"location\": {\"latitude\": ,\"longitude\": },\"extraComputations\": [\"POLLUTANT_CONCENTRATION\"]}";
 const char PROGMEM AQI_URL[] = "https://airquality.googleapis.com/v1/currentConditions:lookup?key=";
 const char PROGMEM WEATHER_URL[] = "https://weather.googleapis.com/v1/currentConditions:lookup?location.latitude=50.018973&location.longitude=19.906229&key=";
 
